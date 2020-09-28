2a83c5464d2cb1ec157249688fa4aa5fadefb965
Bec Sun <rebeccasun86@gmail.com>
2020-09-28 20:14:21 +0100

    name added to portfolio

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,13 +1,41 @@
 import React from 'react'
-import { Container, Avatar, Typography, Grid, Box } from '@material-ui/core'
+import { Container, Typography, Grid, Box, makeStyles, responsiveFontSizes } from '@material-ui/core'
+import Typed from 'react-typed'
 
-
+//CSS Styles
+const useStyles = makeStyles(theme => ({
+  container: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '100vh',
+    textAlign: 'center'
+  },
+  title: {
+    responsiveFontSizes: '50px'
+  },
+  subtitle: {
+    marginBottom: '4rem'
+  }
+}))
 
 
 const Home = () => {
+  const classes = useStyles()
   return (
-    <Box>
-    
+    <Box className={classes.container}>
+      <Typography className={classes.title} variant="h1">
+        <Typed strings={['Bec Sun']} typeSpeed={70}/>
+      </Typography>
+      <br />
+      <Typography className={classes.subtitle} variant="h3">
+        <Typed strings={['Junior', 'Full Stack', 'Developer']}
+          typeSpeed={70}
+          backspeed={60}
+          loop
+        />
+      </Typography>
     </Box>
 
   )
