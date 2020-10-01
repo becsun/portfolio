0733bb83be957242e8bc474f3211946f4a0dfb2e
Bec Sun <rebeccasun86@gmail.com>
2020-10-01 12:01:15 +0100

    projects aligned

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, makeStyles, CardContent } from '@material-ui/core'
+import { Box, makeStyles, Typography } from '@material-ui/core'
 import ScrollableAnchor from 'react-scrollable-anchor'
 import Homepage from '../styles/images/homepage.gif'
 import Goo from '../styles/images/Goo.gif'
@@ -18,29 +18,50 @@ const useStyles = makeStyles({
     marginBottom: 12
   },
   image: {
-    display: 'block',
-    width: '30%',
+    // display: 'relative',
+    display: 'flex',
+    width: '60%',
     height: '100%',
-    paddingBottom: '5%'
+    paddingBottom: '10%'
+  },
+  mainContainer: {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    paddingTop: '10%'
+  },
+  smallContainer: {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
   }
+
 })
 
 const Projects = () => {
   const classes = useStyles()
   return (
+
     <ScrollableAnchor id={'Projects'}>
-      <Box display='flex' flexDirection='column' marginTop={10} width='100%' position="relative">
-        <Box >
+      <Box className={classes.mainContainer}>
+        <Typography>
+          Projects
+        </Typography>
+        <Box className={classes.smallContainer}>
           <img src={Homepage} className={classes.image} alt='project4'/>
         </Box>
-        <Box >
+        <Box className={classes.smallContainer}>
           <img src={Nalu} className={classes.image} alt='project3'/>
         </Box>
 
-        <Box >
+        <Box className={classes.smallContainer}>
           <img src={Alcohol} className={classes.image} alt='project2'/>
         </Box>
-        <Box >
+        <Box className={classes.smallContainer}>
           <img src={Goo} className={classes.image} alt='project1'/>
         </Box>
       </Box>
--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -5,11 +5,27 @@ import {  Box, makeStyles } from '@material-ui/core'
 //CSS Styles
 
 const useStyles = makeStyles({
+  mainBox: {
+    display: 'flex',
+    justifyContent: 'space-evenly'
+  },
   leftLink: {
-    color: 'white'
+    color: 'white',
+    fontSize: '40px',
+    textDecoration: 'none'
+
+
   },
   rightLink: {
-    color: 'white'
+    color: 'white',
+    fontSize: '40px',
+    textDecoration: 'none'
+  },
+  rightBox: {
+    display: 'flex-inline'
+  },
+  leftBox: {
+    display: 'flex-inline'
   }
 })
 
@@ -19,22 +35,20 @@ const Navbar = () => {
   return (
 
     <div>
-      <Box>
-        <a className={classes.leftLink} href='#About'> About </a>
+      <Box className={classes.mainBox}>
+        <Box className={classes.leftBox} >
+          <a className={classes.leftLink} href='#About'> About </a>
+        </Box>
+        <Box className={classes.leftBox}>
+          <a className={classes.leftLink} href='#Projects'> Projects </a>
+        </Box>
+        <Box className={classes.rightBox}>
+          <a className={classes.rightLink} href='#Technologies'> Technologies </a>
+        </Box>
+        <Box className={classes.rightBox}>
+          <a className={classes.rightLink} href='#Contact'> Contact </a>
+        </Box>
       </Box>
-      <Box>
-        <a className={classes.leftLink} href='#Projects'> Projects </a>
-      </Box>
-      <Box>
-        <a className={classes.rightLink} href='#Technologies'> Technologies </a>
-      </Box>
-      <Box>
-        <a className={classes.rightLink} href='#Contact'> Contact </a>
-      </Box>
-
-
-
-
     </div>
 
   )
