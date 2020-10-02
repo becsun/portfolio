30bc024690bbfb66c9db0d68d28700ca86eb660d
Bec Sun <rebeccasun86@gmail.com>
2020-10-02 15:39:30 +0100

    cobtacts added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Box, makeStyles, Typography } from '@material-ui/core'
+import { Icon  } from '@material-ui/core'
+import GitHubIcon from '@material-ui/icons/GitHub'
 import ScrollableAnchor from 'react-scrollable-anchor'
 import Homepage from '../styles/images/homepage.gif'
 import Goo from '../styles/images/Goo.gif'
 import Nalu from '../styles/images/Nalu.png'
 import Alcohol from '../styles/images/Alcohol.png'
+
 
 
 const useStyles = makeStyles({
@@ -52,16 +55,36 @@ const Projects = () => {
           Projects
         </Typography>
         <Box className={classes.smallContainer}>
+          <i className="fas fa-link"/>
+          <Icon size="large" aria-label="github">
+            <GitHubIcon />
+          </Icon>
           <img src={Homepage} className={classes.image} alt='project4'/>
+
         </Box>
+
+
         <Box className={classes.smallContainer}>
+          <i className="fas fa-link"/>
+          <Icon size="large" aria-label="github">
+            <GitHubIcon />
+          </Icon>
           <img src={Nalu} className={classes.image} alt='project3'/>
         </Box>
 
         <Box className={classes.smallContainer}>
+          <i className="fas fa-link"/>
+          <Icon size="large" aria-label="github">
+            <GitHubIcon />
+          </Icon>
           <img src={Alcohol} className={classes.image} alt='project2'/>
         </Box>
+
         <Box className={classes.smallContainer}>
+          <i className="fas fa-link"/>
+          <Icon size="large" aria-label="github">
+            <GitHubIcon />
+          </Icon>
           <img src={Goo} className={classes.image} alt='project1'/>
         </Box>
       </Box>
--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,28 +1,73 @@
 import React from 'react'
 import ScrollableAnchor from 'react-scrollable-anchor'
-import { Icon , Typography } from '@material-ui/core'
+import { Icon , Typography , makeStyles, Box } from '@material-ui/core'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import EmailIcon from '@material-ui/icons/Email'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
 
-const Footer = () => {
 
+const useStyles = makeStyles({
+  h1: {
+    display: 'flex',
+    position: 'relative'
+
+  },
+  box: {
+    display: 'flex',
+    position: 'relative',
+    color: 'white',
+    textDecoration: 'none'
+  },
+  links: {
+    display: 'flex',
+    cursor: 'url',
+    color: 'white',
+    textDecoration: 'none'
+
+  }
+})
+
+
+const Footer = () => {
+  const classes = useStyles()
   return (
     <div>
-      <Typography>
-        <h1>Contact</h1>
-      </Typography>
-      <ScrollableAnchor id={'Contact'}>
-        <Icon size="large" aria-label="github">
-          <GitHubIcon />
-        </Icon>
-      </ScrollableAnchor>
-      <Icon size="large" aria-label="email">
-        <EmailIcon />
-      </Icon>
-      <Icon size="large" aria-label="linkedIn">
-        <LinkedInIcon />
-      </Icon>
+      <Box className={classes.box}>
+        <Typography className={classes.h1}>
+          <h1>Contact</h1>
+        </Typography>
+
+
+        <Box>
+          <ScrollableAnchor id={'Contact'}>
+            <Icon size="large" aria-label="github">
+              <GitHubIcon />
+            </Icon>
+          </ScrollableAnchor>
+          <Typography>
+            <a className={classes.links} href="https://github.com/becsun" target="_blank" rel='noopener  noreferrer' >github.com/becsun</a>
+          </Typography>
+          <br/>
+          <Icon size="large" aria-label="email">
+            <EmailIcon />
+          </Icon>
+          <Typography>
+            <a className={classes.links} href="mailto:becsun@icloud.com" target="_blank" rel='noopener noreferrer' >becsun@icloud.com</a>
+          </Typography>
+          <br/>
+          <Icon size="large" aria-label="linkedIn">
+            <LinkedInIcon />
+          </Icon>
+          <Typography>
+            <a className={classes.links} href="https://www.linkedin.com/in/bec-sun/" target="_blank" rel='noopener  noreferrer'>bec-sun</a>
+          </Typography>
+        </Box>
+
+      </Box>
+
+
+
+
 
     </div>
 
